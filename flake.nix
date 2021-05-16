dbe2b0835aab5fd1f4f9fb3956735499d09f6abf
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-05-17 00:34:46 +0300

    local

--- flake.nix
+++ flake.nix
@@ -13,17 +13,17 @@
       let
         lib = htmlNix.lib.${system};
         ownTemplater = x: x;
-        site = lib.pkgsLib.mkSiteFrom { src = ./.; templater = context: ownTemplater (lib.templaters.basic context); };
+        site = local: lib.pkgsLib.mkSiteFrom { inherit local; src = ./.; templater = context: ownTemplater (lib.templaters.basic context); };
       in
       rec {
         apps = {
           website = mkApp {
-            drv = lib.pkgsLib.mkServeFromSite site;
+            drv = lib.pkgsLib.mkServeFromSite (site true);
             name = "serve";
           };
         };
         packages = {
-          website = lib.pkgsLib.mkSitePath site;
+          website = lib.pkgsLib.mkSitePath (site false);
         };
         defaultPackage = packages.website;
         defaultApp = apps.website;
