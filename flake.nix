f93c66c0def7806039ca36860836a4a85ff35d56
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2024-08-15 12:28:51 +0300

    build: add nix

---
+++ flake.nix
@@ -0,0 +1,90 @@
+{
+  inputs.parts.url = "github:hercules-ci/flake-parts";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.systems.url = "github:nix-systems/x86_64-linux";
+  inputs.naked-shell.url = "github:yusdacra/mk-naked-shell";
+
+  outputs = inp:
+    inp.parts.lib.mkFlake {inputs = inp;} {
+      systems = import inp.systems;
+      imports = [
+        inp.naked-shell.flakeModule
+      ];
+      perSystem = {
+        config,
+        pkgs,
+        ...
+      }: let
+        packageJson = builtins.fromJSON (builtins.readFile ./package.json);
+      in {
+        devShells.default = config.mk-naked-shell.lib.mkNakedShell {
+          name = "gazesys-devshell";
+          packages = with pkgs; [
+            nodejs-slim_latest bun
+            nodePackages.svelte-language-server
+            nodePackages.typescript-language-server
+          ];
+          shellHook = ''
+            export PATH="$PATH:$PWD/node_modules/.bin"
+          '';
+        };
+        packages.gazesys-modules = pkgs.stdenv.mkDerivation {
+          pname = "${packageJson.name}-modules";
+          version = packageJson.version;
+
+          src = ./.;
+          
+          outputHash = "sha256-yZRtcXr+yYiuq9bIDwx/C/7T22VCaJ/d3Lv4kdOjHnk=";
+          outputHashAlgo = "sha256";
+          outputHashMode = "recursive";
+
+          nativeBuildInputs = with pkgs; [bun];
+
+          dontConfigure = true;
+          impureEnvVars = pkgs.lib.fetchers.proxyImpureEnvVars
+            ++ [ "GIT_PROXY_COMMAND" "SOCKS_SERVER" ];
+
+          buildPhase = "bun install --no-progress --frozen-lockfile";
+          installPhase = "mv node_modules $out";
+        };
+        packages.gazesys = pkgs.stdenv.mkDerivation {
+          pname = packageJson.name;
+          version = packageJson.version;
+
+          src = ./.;
+
+          nativeBuildInputs = [pkgs.makeBinaryWrapper];
+          buildInputs = [pkgs.bun];
+
+          PUBLIC_BASEURL="localhost:5173";
+
+          configurePhase = ''
+            runHook preConfigure
+            cp -R ${config.packages.gazesys-modules} node_modules
+            substituteInPlace node_modules/.bin/vite \
+              --replace "/usr/bin/env node" "${pkgs.nodejs-slim_latest}/bin/node"
+            runHook postConfigure
+          '';
+          buildPhase = ''
+            runHook preBuild
+            bun --prefer-offline run build
+            runHook postBuild
+          '';
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/bin
+            ln -s ${config.packages.gazesys-modules} $out
+            cp -R ./build/* $out
+
+            makeBinaryWrapper ${pkgs.bun}/bin/bun $out/bin/${packageJson.name} \
+              --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.bun ]} \
+              --add-flags "run --prefer-offline --no-install --cwd $out start"
+
+            runHook postInstall
+          '';
+        };
+        packages.default = config.packages.gazesys;
+      };
+    };
+}
