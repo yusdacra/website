4c563b06779afec2057563e6f48e72893954bbb0
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2024-08-15 14:32:16 +0300

    fix: enable trailing slashes

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,7 @@ import { dev } from '$app/environment';
 export const csr = dev;
 export const ssr = true;
 export const prerender = true;
+export const trailingSlash = 'always';
 
 export async function load({ url }) {
     return { route: url.pathname }
