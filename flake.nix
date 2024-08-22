c3fcb13b63c2d7ffa96189d8f178bafbae48e8c6
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2024-08-23 01:55:30 +0300

    build: add guestbook server build (scala)

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.systems.url = "github:nix-systems/x86_64-linux";
   inputs.naked-shell.url = "github:yusdacra/mk-naked-shell";
+  inputs.sbt-derivation.url = "github:zaninime/sbt-derivation";
+  inputs.sbt-derivation.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = inp:
     inp.parts.lib.mkFlake {inputs = inp;} {
@@ -12,9 +14,10 @@
       ];
       perSystem = {
         config,
-        pkgs,
+        system,
         ...
       }: let
+        pkgs = inp.nixpkgs.legacyPackages.${system}.appendOverlays [inp.sbt-derivation.overlays.default];
         packageJson = builtins.fromJSON (builtins.readFile ./package.json);
       in {
         devShells.default = config.mk-naked-shell.lib.mkNakedShell {
@@ -82,6 +85,35 @@
               --add-flags "run --prefer-offline --no-install --cwd $out start"
 
             runHook postInstall
+          '';
+        };
+        packages.guestbook-jar = pkgs.mkSbtDerivation rec {
+          pname = "guestbook";
+          version = "0.0.1-jar";
+          depsSha256 = "sha256-hIGZTEZpqgBuo6VGgZ6dQd5kK0RLVNy5REVBBYA3Gak=";
+          src = ./guestbook;
+          buildPhase = ''
+            sbt assembly
+            mkdir -p ./target/graalvm-native-image
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp target/scala-3.4.2/${pname}-assembly-0.0.1-SNAPSHOT.jar $out/${pname}.jar
+          '';
+          nativeBuildInputs = [
+            pkgs.graalvm-ce
+          ];
+        };
+        packages.guestbook = pkgs.stdenv.mkDerivation rec {
+          inherit (config.packages.guestbook-jar) pname nativeBuildInputs;
+          version = "0.0.1";
+          src = config.packages.guestbook-jar;
+          buildPhase = ''
+            native-image -H:+ReportExceptionStackTraces -H:+AddAllCharsets --allow-incomplete-classpath --no-fallback --initialize-at-run-time --enable-http --enable-all-security-services --verbose -jar "./${pname}.jar" ./${pname}
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ${pname} $out/bin/${pname}
           '';
         };
         packages.default = config.packages.gazesys;
