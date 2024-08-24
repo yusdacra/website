aee545c96326731f642909920bc6cf92a8514675
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2024-08-24 15:50:02 +0300

    fix: never cache anything

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,7 @@ export const ssr = true;
 export const prerender = false;
 export const trailingSlash = 'always';
 
-export async function load({ url }) {
+export async function load({ url, setHeaders }) {
+    setHeaders({'Cache-Control': 'no-cache'})
     return { route: url.pathname }
 }
