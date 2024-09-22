8e850afea8330be463cb994ff3a31ecbac31c611
dusk <y.bera003.06@protonmail.com>
2024-09-22 22:03:24 +0300

    fix: dont use no-store

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,6 +4,6 @@ export const prerender = true;
 export const trailingSlash = 'always';
 
 export async function load({ url, setHeaders }) {
-    setHeaders({'Cache-Control': 'no-store'})
+    setHeaders({'Cache-Control': 'no-cache'})
     return { route: url.pathname }
 }
