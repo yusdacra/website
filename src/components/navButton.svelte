2fcffed6f17592e5154e1a7ec14e6e85f0ad1193
dusk <y.bera003.06@protonmail.com>
2024-09-23 16:14:15 +0300

    refactor: remove a div in navbutton

--- src/components/navButton.svelte
+++ src/components/navButton.svelte
@@ -5,26 +5,23 @@
 	export let iconUri: string;
 </script>
 
-<div
+<a
 	class="
-    max-w-36 p-0.5 pr-1.5 border-ralsei-white border-4
-    {highlight
+	max-w-36 p-0.5 pr-1.5 border-ralsei-white border-4
+	{highlight
 		? 'min-w-36 bg-gradient-to-l to-ralsei-pink-neon/30 from-ralsei-pink-regular/20 from-30% border-ridge motion-safe:animate-pulse hover:animate-none'
 		: 'w-fit border-double hover:border-solid motion-safe:hover:animate-bounce hover:underline'}
-"
+	flex gap-1 items-center justify-center align-middle text-center
+	{highlight ? 'text-ralsei-pink-regular app-selected-route' : 'text-ralsei-green-light'}
+	"
+	title={name}
+	href="/{href}"
+	data-sveltekit-preload-data="hover"
 >
-	<a
-		class="
-            flex gap-1 items-center justify-center align-middle text-center h-full
-            {highlight
-			? 'text-ralsei-pink-regular app-selected-route'
-			: 'text-ralsei-green-light'}
-        "
-		title={name}
-		href="/{href}"
-		data-sveltekit-preload-data="hover"
+	<img class="max-w-4" style="image-rendering: pixelated;" src={iconUri} alt={name} />
+	<div
+		class="font-monospace text-sm/3 overflow-hidden text-ellipsis text-nowrap [text-decoration-line:inherit]"
 	>
-		<img class="max-w-4" style="image-rendering: pixelated;" src={iconUri} alt={name} />
-		<div class="font-monospace text-sm/3 overflow-hidden text-ellipsis text-nowrap [text-decoration-line:inherit]">{name}</div>
-	</a>
-</div>
+		{name}
+	</div>
+</a>
