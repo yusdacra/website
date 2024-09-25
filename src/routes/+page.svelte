8797e2cc4df99b1dcbb0464bd76ea9b0eed437a0
dusk <y.bera003.06@protonmail.com>
2024-09-26 00:36:54 +0300

    feat: add image :3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,26 +6,26 @@
 <div class="flex flex-col md:flex-row gap-y-4 lg:gap-y-0 md:h-full h-card">
 	<div class="flex flex-col gap-y-2 lg:gap-y-0">
 		<Window title="readme?" iconUri="/icons/question.png">
-			<div class="prose prose-ralsei prose-img:m-0 leading-6">
-				<p>
+			<div class="flex flex-col prose prose-ralsei prose-img:m-0 leading-none">
+				<div class="flex flex-grow">
+					<img class="mt-1 ml-1 w-36 h-36 u-photo" src="/pfp.png" alt="character from q.u.q." title="character from q.u.q. (good vn go read it NOW)"/>
+					<ul class="place-self-center m-0 leading-none marker:[content:'->'] [list-style-type:'->']">
+						<li class="[list-style-type:'->'] p-note">trying to do stuff</li>
+						<li class="[list-style-type:'->']"><span class="p-category">software engineer</span>, <span class="p-category">indie game dev</span></li>
+						<li class="[list-style-type:'->']"><span class="p-gender-identity">genderfluid</span></li>
+						<li class="[list-style-type:'->']">in <span class="p-country-name">turkey</span> <i class="text-[0.5rem]">(get me out)</i></li>
+						<li class="[list-style-type:'->']">aka <span class="p-nickname">yusdacra</span></li>
+					</ul>
+				</div>
+				<p class="leading-none m-0 mt-3 text-sm">
 					hi there
 					<img
-						class="relative inline h-6 animate-squiggle pb-1 pr-1"
+						class="relative inline h-5 animate-squiggle pb-1 pr-1"
 						src="/wavey.gif"
 						alt="wavey"
 						title="hi :33"
 					/>
-					<i>i'm <a class="p-name u-url u-uid" href={PUBLIC_BASE_URL}>dusk</a>!</i>
-				</p>
-				<ul class="marker:[content:'->'] leading-none">
-					<li class="p-note">trying to do stuff</li>
-					<li><span class="p-category">software engineer</span>, <span class="p-category">indie game dev</span></li>
-					<li><span class="p-gender-identity">genderfluid</span></li>
-					<li>in <span class="p-country-name">turkey</span> <i class="text-[0.5rem] align-text-bottom">(get me out)</i></li>
-					<li>aka <span class="p-nickname">yusdacra</span></li>
-				</ul>
-				<p>
-					...and this is my personal spot on the web where i plan to put everything that i do on (<i class="text-sm">if i don't forget to</i> 👍)
+					<i>i'm <a class="m-0 p-0 p-name u-url u-uid" href={PUBLIC_BASE_URL}>dusk</a>!</i>
 				</p>
 			</div>
 		</Window>
