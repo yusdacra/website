64f2864617fad2b73d529ac85a36b9c5bbdf4f82
dusk <y.bera003.06@protonmail.com>
2024-09-29 21:53:59 +0300

    fix: navbuttons now bounce in reverse
    this makes it so that the user cant trigger a reset of the animation by just placing the cursor at the bottom and the button going out of the cursors area

--- src/components/navButton.svelte
+++ src/components/navButton.svelte
@@ -10,7 +10,7 @@
 	max-w-36 p-0.5 pr-1.5 border-ralsei-white border-4
 	{highlight
 		? 'min-w-36 bg-gradient-to-l to-ralsei-pink-neon/30 from-ralsei-pink-regular/20 from-30% border-ridge motion-safe:animate-pulse hover:animate-none'
-		: 'w-fit border-double hover:border-solid motion-safe:hover:animate-bounce hover:underline'}
+		: 'w-fit border-double hover:border-solid animate-bounce-reverse hover:underline'}
 	flex gap-1 items-center justify-center align-middle text-center
 	{highlight ? 'text-ralsei-pink-regular app-selected-route' : 'text-ralsei-green-light'}
 	"
