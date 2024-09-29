61c7d27e2243d64791e93c186db0da5c945064cd
dusk <y.bera003.06@protonmail.com>
2024-09-29 22:38:01 +0300

    feat: remove the animation warning

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,12 +8,27 @@
 		<Window title="readme?" iconUri="/icons/question.png">
 			<div class="flex flex-col prose prose-ralsei prose-img:m-0 leading-none">
 				<div class="flex flex-grow">
-					<img class="mt-1 ml-1 w-36 [height:9rem] u-photo" src="/pfp.png" alt="character from q.u.q." title="character from q.u.q. (good vn go read it NOW)"/>
-					<ul class="place-self-center m-0 leading-none marker:[content:'->'] [list-style-type:'->']">
+					<img
+						class="mt-1 ml-1 w-36 [height:9rem] u-photo"
+						src="/pfp.png"
+						alt="character from q.u.q."
+						title="character from q.u.q. (good vn go read it NOW)"
+					/>
+					<ul
+						class="place-self-center m-0 leading-none marker:[content:'->'] [list-style-type:'->']"
+					>
 						<li class="[list-style-type:'->'] p-note">trying to do stuff</li>
-						<li class="[list-style-type:'->']"><span class="p-category">software engineer</span>, <span class="p-category">indie game dev</span></li>
-						<li class="[list-style-type:'->']"><span class="p-gender-identity">genderfluid</span></li>
-						<li class="[list-style-type:'->']">in <span class="p-country-name">turkey</span> <i class="text-[0.5rem]">(get me out)</i></li>
+						<li class="[list-style-type:'->']">
+							<span class="p-category">software engineer</span>,
+							<span class="p-category">indie game dev</span>
+						</li>
+						<li class="[list-style-type:'->']">
+							<span class="p-gender-identity">genderfluid</span>
+						</li>
+						<li class="[list-style-type:'->']">
+							in <span class="p-country-name">turkey</span>
+							<i class="text-[0.5rem]">(get me out)</i>
+						</li>
 						<li class="[list-style-type:'->']">aka <span class="p-nickname">yusdacra</span></li>
 					</ul>
 				</div>
@@ -25,72 +40,66 @@
 						alt="wavey"
 						title="hi :33"
 					/>
-					<i>i'm <a class="m-0 [padding:0px] p-name u-url u-uid" href={PUBLIC_BASE_URL}>dusk</a>!</i>
+					<i>i'm <a class="m-0 [padding:0px] p-name u-url u-uid" href={PUBLIC_BASE_URL}>dusk</a>!</i
+					>
 				</p>
 			</div>
 		</Window>
-		<div class="grow" />
-		<Window title="latest stuff">
-			<div class="prose prose-ralsei prose-img:m-0 leading-6">
-				<p>
-					worked on a game for a jam!! check it out at <a href="https://manonox.itch.io/fish-well"
-						>it's itch.io page</a
-					>!
-				</p>
-				<img
-					src="https://img.itch.zone/aW1hZ2UvMjkxNjQzOC8xNzQ2MDY2Mi5wbmc=/original/%2BIb5Vf.png"
-					alt="fish well gameplay screenshot"
-				/>
+		<div class="mt-auto">
+			<Window title="latest stuff">
+				<div class="prose prose-ralsei prose-img:m-0 leading-6">
+					<p>
+						worked on a game for a jam!! check it out at <a href="https://manonox.itch.io/fish-well"
+							>it's itch.io page</a
+						>!
+					</p>
+					<img
+						class="md:max-w-[35rem]"
+						src="https://img.itch.zone/aW1hZ2UvMjkxNjQzOC8xNzQ2MDY2Mi5wbmc=/original/%2BIb5Vf.png"
+						alt="fish well gameplay screenshot"
+					/>
+				</div>
+			</Window>
+		</div>
+	</div>
+	<div class="hidden md:block md:grow" />
+	<div class="flex flex-col gap-y-2 lg:gap-y-0">
+		<Window title="contacts!" iconUri="/icons/contact.png">
+			<div class="prose prose-ralsei leading-6">
+				<ul>
+					<li>discord: yusdacra</li>
+					<li>
+						e-mail:
+						<a class="u-email" href="mailto:y.bera003.06@pm.me" rel="me">y.bera003.06@pm.me</a>
+					</li>
+				</ul>
+				<h3>development</h3>
+				<ul>
+					<li>
+						github:
+						<a class="u-url" href="https://github.com/yusdacra" rel="me">@yusdacra</a>
+					</li>
+					<li>
+						gitlab:
+						<a class="u-url" href="https://gitlab.com/yusdacra" rel="me">@yusdacra</a>
+					</li>
+					<li>
+						my gitea:
+						<a class="u-url" href="https://git.gaze.systems/dusk" rel="me">@dusk</a>
+					</li>
+				</ul>
+				<h3>socials</h3>
+				<ul>
+					<li>
+						bluesky:
+						<a class="u-url" href="https://bsky.app/profile/gaze.systems" rel="me">@gaze.systems</a>
+					</li>
+					<li>
+						misskey.art:
+						<a class="u-url" href="https://misskey.art/@yusdacra" rel="me">@yusdacra</a>
+					</li>
+				</ul>
 			</div>
 		</Window>
 	</div>
-	<div class="hidden md:block md:grow" />
-	<Window title="contacts!" iconUri="/icons/contact.png">
-		<div class="prose prose-ralsei leading-6">
-			<ul>
-				<li>discord: yusdacra</li>
-				<li>
-					e-mail:
-					<a class="u-email" href="mailto:y.bera003.06@pm.me" rel="me">y.bera003.06@pm.me</a>
-				</li>
-			</ul>
-			<h3>development</h3>
-			<ul>
-				<li>
-					github:
-					<a class="u-url" href="https://github.com/yusdacra" rel="me">@yusdacra</a>
-				</li>
-				<li>
-					gitlab:
-					<a class="u-url" href="https://gitlab.com/yusdacra" rel="me">@yusdacra</a>
-				</li>
-				<li>
-					my gitea:
-					<a class="u-url" href="https://git.gaze.systems/dusk" rel="me">@dusk</a>
-				</li>
-			</ul>
-			<h3>socials</h3>
-			<ul>
-				<li>
-					bluesky:
-					<a class="u-url" href="https://bsky.app/profile/gaze.systems" rel="me">@gaze.systems</a>
-				</li>
-				<li>
-					misskey.art:
-					<a class="u-url" href="https://misskey.art/@yusdacra" rel="me">@yusdacra</a>
-				</li>
-			</ul>
-		</div>
-	</Window>
-</div>
-
-<div class="hidden motion-reduce:block absolute max-w-[40ch] bottom-0 right-0 md:bottom-[10vh] md:right-[8vw]">
-	<Window title=":(" iconUri="/icons/warning.png">
-		<div class="prose prose-ralsei leading-6">
-			<p>
-				you have <code>prefers-reduced-motion</code> enabled so i disabled (most of) the animations!
-			</p>
-			<p class="text-xs italic">they looked nice i think....</p>
-		</div>
-	</Window>
 </div>
