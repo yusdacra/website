e86cfc1d238c1f63c10f0533619246bd768e069e
dusk <y.bera003.06@protonmail.com>
2024-10-23 08:28:49 +0900

    build: add a script for easier deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set +x
+
+git commit -m "$1" && git push
+git tag -f latest && git push -f --tags
+
+sleep 15s
+
+cd $HOME/ark
+nix flake update blog
+nix run .#nh -- os build -H wolumonde .
+nix run .#apps.nixinate.wolumonde -L --show-trace
