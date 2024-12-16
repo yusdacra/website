b2490bb337cbb718ea952cbe2c185beb0fa6bc97
dusk <y.bera003.06@protonmail.com>
2024-12-16 17:43:43 +0300

    feat: add replyTo for bot

--- gazebot/src/main.rs
+++ gazebot/src/main.rs
@@ -1,10 +1,12 @@
 use reqwest::header::AUTHORIZATION;
-use serenity::all::ActivityData;
-use serenity::async_trait;
-use serenity::json::json;
-use serenity::model::channel::Message;
-use serenity::model::prelude::*;
-use serenity::prelude::*;
+use scc::HashMap as ConcurrentHashMap;
+use serenity::{
+    all::ActivityData,
+    async_trait,
+    json::Value as JsonValue,
+    model::{channel::Message, prelude::*},
+    prelude::*,
+};
 use shuttle_runtime::SecretStore;
 
 const ADMIN_USER_ID: u64 = 853064602904166430;
@@ -12,6 +14,7 @@ const ADMIN_USER_ID: u64 = 853064602904166430;
 struct Handler {
     http: reqwest::Client,
     secrets: SecretStore,
+    notes: ConcurrentHashMap<MessageId, String>,
 }
 
 #[async_trait]
@@ -26,14 +29,29 @@ impl EventHandler for Handler {
         }
 
         let mut note_content = msg.content.clone();
-        
+
         const BSKY_TAG: &str = ".nobsky";
         let no_bsky_posse = note_content.contains(BSKY_TAG);
         if no_bsky_posse {
             note_content = note_content.replace(BSKY_TAG, "");
         }
 
-        let note_data = json!({"content": note_content.trim(), "bskyPosse": !no_bsky_posse});
+        let mut note_data = serenity::json::JsonMap::new();
+        note_data.insert(
+            "content".to_string(),
+            JsonValue::String(note_content.trim().to_string()),
+        );
+        note_data.insert("bskyPosse".to_string(), JsonValue::Bool(!no_bsky_posse));
+        // add replyTo if we are replying to a previous message
+        if let Some(reply_msg) = msg.referenced_message.as_deref() {
+            if let Some(reply_note_id) = self
+                .notes
+                .read_async(&reply_msg.id, |_, v| v.to_owned())
+                .await
+            {
+                note_data.insert("replyTo".to_string(), JsonValue::String(reply_note_id));
+            }
+        }
         let resp = self
             .http
             .post("https://gaze.systems/log/create")
@@ -42,7 +60,7 @@ impl EventHandler for Handler {
             .send()
             .await
             .and_then(|resp| resp.error_for_status());
-        
+
         let resp = match resp {
             Ok(r) => r,
             Err(why) => {
@@ -52,10 +70,17 @@ impl EventHandler for Handler {
         };
 
         let note_resp = resp.json::<serenity::json::JsonMap>().await.unwrap();
-        let created_note_id = note_resp["noteId"].as_str().expect("note id must be a string");
+        let created_note_id = note_resp["noteId"]
+            .as_str()
+            .expect("note id must be a string");
         tracing::info!("succesfully created note with id {created_note_id}");
 
-        let mut reply_content = format!("created log at https://gaze.systems/log?id={created_note_id}");
+        self.notes
+            .upsert_async(msg.id, created_note_id.to_string())
+            .await;
+
+        let mut reply_content =
+            format!("created log at https://gaze.systems/log?id={created_note_id}");
         let errors = note_resp["errors"].as_array().expect("must be array");
         if !errors.is_empty() {
             reply_content.push_str("\n\nerrors:");
@@ -90,6 +115,7 @@ async fn serenity(
         .event_handler(Handler {
             http: reqwest::Client::new(),
             secrets,
+            notes: ConcurrentHashMap::new(),
         })
         .await
         .expect("Err creating client");
