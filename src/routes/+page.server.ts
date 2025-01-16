48727b60e7a0751de56b956868d6d4359fa93aa4
dusk <y.bera003.06@protonmail.com>
2025-01-16 14:56:10 +0300

    feat: add new blinkies, make getting the same blinkie more rare

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,10 +4,18 @@ import { steamGetNowPlaying } from "$lib/steam"
 export const load = async ({}) => {
     const lastTrack = await lastFmGetNowPlaying()
     const lastGame = await steamGetNowPlaying()
-    const banners = [getBannerNo(), getBannerNo(), getBannerNo()]
+    let banners: number[] = []
+    while (banners.length < 3) {
+        const no = getBannerNo(banners)
+        banners.push(no)
+    }
     return {banners, lastTrack, lastGame}
 }
 
-const getBannerNo = () => {
-    return Math.floor(Math.random() * 18) + 1;
+const getBannerNo = (others: number[]) => {
+    const no = Math.floor(Math.random() * 20) + 1
+    if (others.includes(no)) {
+        return ((no + (Math.floor(Math.random() * 20))) % 20) + 1
+    }
+    return no
 };
