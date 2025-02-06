994bd60eea0993ab582352e5c32403e416f7b591
dusk <y.bera003.06@protonmail.com>
2025-02-07 06:04:36 +0900

    build: remove deploy sleep

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,6 @@ set -x
 git commit -m "$1" && git push
 git tag -f latest && git push -f --tags
 
-sleep 15s
-
 cd $HOME/ark
 nix flake update blog
 nix run .#nh -- os build -H wolumonde . && nix run .#apps.nixinate.wolumonde -L --show-trace
