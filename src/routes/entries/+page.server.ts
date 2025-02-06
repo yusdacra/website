4b81de80d52cbddd7395faf19a7d5e7657771b97
dusk <y.bera003.06@protonmail.com>
2025-02-06 23:12:06 +0300

    refactor: remove unused logs code

--- src/routes/entries/+page.server.ts
+++ src/routes/entries/+page.server.ts
@@ -1,15 +1,3 @@
 import {_load as load_logs} from '../log/+page.server.ts'
 
-export const load = (params) => {
-    var url = params.url
-    var log_id = url.searchParams.get("log_id")
-    if (log_id !== null) {
-        url.searchParams.append("id", log_id)
-    }
-    var log_page = url.searchParams.get("log_page")
-    if (log_page !== null) {
-        url.searchParams.append("page", log_page)
-    }
-    var logs_result = load_logs()
-    return logs_result
-}
+export const load = load_logs
