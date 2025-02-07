84382e5b051e62a4f1a306e122b975c06fbd29f3
dusk <y.bera003.06@protonmail.com>
2025-02-07 07:59:39 +0300

    chore: remove guestbook, gazebot (now unused)

--- flake.nix
+++ flake.nix
@@ -3,8 +3,6 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.systems.url = "github:nix-systems/x86_64-linux";
   inputs.naked-shell.url = "github:yusdacra/mk-naked-shell";
-  inputs.sbt-derivation.url = "github:zaninime/sbt-derivation";
-  inputs.sbt-derivation.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = inp:
     inp.parts.lib.mkFlake {inputs = inp;} {
@@ -17,7 +15,7 @@
         system,
         ...
       }: let
-        pkgs = inp.nixpkgs.legacyPackages.${system}.appendOverlays [inp.sbt-derivation.overlays.default];
+        pkgs = inp.nixpkgs.legacyPackages.${system};
         packageJson = builtins.fromJSON (builtins.readFile ./package.json);
       in {
         devShells.default = config.mk-naked-shell.lib.mkNakedShell {
@@ -26,7 +24,6 @@
             nodejs-slim_latest bun
             nodePackages.svelte-language-server
             nodePackages.typescript-language-server
-            sbt
           ];
           shellHook = ''
             export PATH="$PATH:$PWD/node_modules/.bin"
@@ -88,35 +85,6 @@
               --add-flags "run --bun --prefer-offline --no-install --cwd $out start"
 
             runHook postInstall
-          '';
-        };
-        packages.guestbook-jar = pkgs.mkSbtDerivation rec {
-          pname = "guestbook";
-          version = "0.0.1-jar";
-          depsSha256 = "sha256-hIGZTEZpqgBuo6VGgZ6dQd5kK0RLVNy5REVBBYA3Gak=";
-          src = ./guestbook;
-          buildPhase = ''
-            sbt assembly
-            mkdir -p ./target/graalvm-native-image
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp target/scala-3.4.2/${pname}-assembly-0.0.1-SNAPSHOT.jar $out/${pname}.jar
-          '';
-          nativeBuildInputs = [
-            pkgs.graalvm-ce
-          ];
-        };
-        packages.guestbook = pkgs.stdenv.mkDerivation rec {
-          inherit (config.packages.guestbook-jar) pname nativeBuildInputs;
-          version = "0.0.1";
-          src = config.packages.guestbook-jar;
-          buildPhase = ''
-            native-image -H:+ReportExceptionStackTraces -H:+AddAllCharsets --allow-incomplete-classpath --no-fallback --initialize-at-run-time --enable-http --enable-all-security-services --verbose -jar "./${pname}.jar" ./${pname}
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            cp ${pname} $out/bin/${pname}
           '';
         };
         packages.default = config.packages.gazesys;
--- guestbook/project/plugins.sbt
+++
@@ -1,4 +0,0 @@
-addSbtPlugin("org.typelevel" % "sbt-tpolecat" % "0.5.1")
-addSbtPlugin("io.spray" % "sbt-revolver" % "0.10.0")
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "2.2.0")
-addSbtPlugin("com.github.sbt" % "sbt-native-packager" % "1.10.4")
