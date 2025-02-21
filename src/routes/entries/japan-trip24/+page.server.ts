980e07b29edb99ecaa5ff6372838c6f59327794c
dusk <y.bera003.06@protonmail.com>
2025-02-21 17:08:22 +0300

    fix: use full image url for cloudinary

--- src/routes/entries/japan-trip24/+page.server.ts
+++ src/routes/entries/japan-trip24/+page.server.ts
@@ -3,10 +3,7 @@ import images from './images.json'
 export async function load({}) {
 	return {
         images: images.map((id) => {
-            return {
-                og: `https://res.cloudinary.com/dgtwf7mar/image/upload/${id}`,
-                id,
-            }
+            return `https://res.cloudinary.com/dgtwf7mar/image/upload/v1/${id}`
         })
     };
 }
