8c75080abe6c733b20a81722252bb4b8062b8b9e
dawn <90008@gaze.systems>
2026-03-15 21:24:03 +0300

    hide hydrant from github activityy

--- eunomia/src/lib/activity.ts
+++ eunomia/src/lib/activity.ts
@@ -100,9 +100,16 @@ const parseFeedToActivity = async (url: string) => {
 		// TODO: probably can implement a deduplication algorithm
 		if (
 			source === 'github' &&
-			['90-008/ark', '90-008/website', 'ark', 'website', 'trill', 'faunu', 'nucleus'].some((repo) =>
-				description.includes(repo)
-			)
+			[
+				'90-008/ark',
+				'90-008/website',
+				'ark',
+				'website',
+				'trill',
+				'faunu',
+				'nucleus',
+				'hydrant'
+			].some((repo) => description.includes(repo))
 		)
 			continue;
 		// dont show activity that is just chore
